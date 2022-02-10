a41fde00924bd9b22356a72f2e0b85564e06dbd6
Valdir Lima <valdirlima508@gmail.com>
2022-02-10 16:31:39 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace VariableExersize
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+
+            string catName = "Garfield";
+            int catAge = 5;
+            char middleInitial = 'V';
+            bool isBald = false;
+            double exactAge = 5.7;
+            decimal myMoney = 100.000000454m;
+
+            Console.WriteLine(catName);
+            Console.WriteLine(catAge);
+            Console.WriteLine(middleInitial);
+            Console.WriteLine(isBald);
+            Console.WriteLine(exactAge);
+            Console.WriteLine(myMoney);
+            Console.WriteLine($"My dogs name is {catName}, He is {catAge} years old, His middle initial is {middleInitial}, Many people think my cat is bald but that is {isBald}, \n" +
+                $"The vet told me his exact age is {exactAge}, I currently have {myMoney} dollars");
+
+
+
+
+        }
+    }
+}
